6934942ba73c6353c4fb879ad06b626bcca63f67
Justin Mac <macjustin1@aol.com>
2016-04-30 16:06:16 -0700

    Initial Commit

---
+++ Now Or Never 2.0Tests/Now_Or_Never_2_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Now_Or_Never_2_0Tests.swift
+//  Now Or Never 2.0Tests
+//
+//  Created by Justin Mac on 4/30/16.
+//  Copyright © 2016 Justin Mac. All rights reserved.
+//
+
+import XCTest
+@testable import Now_Or_Never_2_0
+
+class Now_Or_Never_2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

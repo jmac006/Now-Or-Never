fd2970858038c3c2d2b76670c658bf6e5064a117
Justin Mac <macjustin1@aol.com>
2016-05-04 19:52:24 -0700

    added Person class, have yet to implement Person to ViewController

--- Now Or Never 2.0/Person.swift
+++ Now Or Never 2.0/Person.swift
@@ -11,17 +11,44 @@ import UIKit
 class Person {
     
     var name = String()
+    var bio = String()
+    var id = Int() //unique identifier for every person
     var age = Int()
-    var likes = [Person]() //stores who the person likes in this array
+    var crushes = [Person]() //stores who the person likes in this array
     var matches = [Person]() //stores the matches the person has
     
-    /*func checkLikes(Person other) -> Bool {
-        for like in likes {
-            if(like == other) {
+    init() {
+        name = "No name"
+        id = -1;
+        age = 100
+        bio = "Empty Bio"
+    }
+    
+    init(name: String, id: Int, age : Int, bio : String) {
+        self.name = name
+        self.age = age
+        self.bio = bio
+    }
+    
+    func addCrush(p : Person) -> Void {
+        crushes.append(p)
+    }
+    
+    
+    func checkLikes(p : Person) -> Bool { //checks if the person liked them or not
+        for crush in crushes {
+            if(crush == p) {
                 return true
             }
         }
         return false
-    }*/
+    }
     
+}
+
+func ==(left : Person, right : Person) -> Bool { //overrides the equality operator
+    if (left.name == right.name && left.id == right.id) {
+        return true
+    }
+    return false
 }
